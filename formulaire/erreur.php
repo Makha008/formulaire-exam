4f9ed3283eedc27391e80aad91828a5216aaccc6
Makha008 <makhadiop008@gmail.com>
2018-03-30 21:21:51 +0100

    formulaire

---
+++ formulaire/erreur.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+
+<html>
+
+
+<head>
+
+	<title>Lister Les elements de la base de donnée</title>
+
+
+	<META charset="utf-8"/>
+
+</head>
+
+
+ <body>
+
+
+
+  <?php
+
+
+
+          echo "Une erreur s'est produite lors de l'insertion en base bang.";
+
+
+
+  ?>
+
+
+
+</body>
+
+</html>
